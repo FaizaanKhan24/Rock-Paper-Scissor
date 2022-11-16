0bea5e1ef2f9e770ab950aeeddd3a3335383e2e8
Faizaan Khan <faizaankhan24@gmail.com>
2022-11-15 23:57:37 -0500

    Initial Commit

---
+++ Rock Paper Scissor/Rock_Paper_ScissorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Rock_Paper_ScissorApp.swift
+//  Rock Paper Scissor
+//
+//  Created by Faizaan Khan on 11/15/22.
+//
+
+import SwiftUI
+
+@main
+struct Rock_Paper_ScissorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
