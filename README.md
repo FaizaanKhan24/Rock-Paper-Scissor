731b87841e47fa47d1f2d2a1de5255834dfdac46
Faizaan Khan <faizaankhan24@gmail.com>
2022-11-17 16:57:31 -0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Rock Paper Scissor
+
+## #100DaysOfSwiftUI - Challenge Project
+
+A SwiftUI game, that allows user to select the three options from by clicking on buttons provided for each option.
+
+- The user will play against the computer 
+- The user gets 5 turns to get the best score
+
+### User Interface 
+
+![Rock Paper Scissor iOS UI](/Users/faizaankhan/Desktop/Simulator Screen Shot - iPhone 14 Pro - 2022-11-17 at 14.27.11.png)
+ 
--- Rock Paper Scissor/ContentView.swift
+++ Rock Paper Scissor/ContentView.swift
@@ -8,14 +8,161 @@
 import SwiftUI
 
 struct ContentView: View {
+    let userOptions = ["Rock", "Paper", "Scissor"]
+    let counterOptions = ["Paper", "Scissor", "Rock"]
+    
+    @State private var userChoice: Int = 0
+    @State private var compChoice: Int = 0
+    @State private var userScore: Int = 0
+    @State private var compScore: Int = 0
+    @State private var turns: Int = 0
+    
+    @State private var turnStatus: String = ""
+    
+    @State private var isShowMove: Bool = false
+    @State private var isGameOver: Bool = false
+    @State private var isturnWon: Bool = false
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack{
+            Color(red: 0.3, green: 0.2, blue: 0.75)
+                .ignoresSafeArea()
+            
+            VStack {
+                
+                Spacer()
+                
+                Text("Rock-Paper-Scissors")
+                    .foregroundColor(.white)
+                    .font(.largeTitle.weight(.bold))
+                
+                Spacer()
+                Spacer()
+                
+                HStack(spacing: 25){
+                    VStack(spacing: 15){
+                        Text("Your Score:")
+                            .foregroundColor(.yellow)
+                            .font(.system(size: 25).weight(.light))
+                        Text("\(userScore)")
+                            .foregroundColor(.yellow)
+                            .font(.system(size: 50).weight(.bold))
+                    }
+                    
+                    VStack(spacing: 15){
+                        Text("Comp Score:")
+                            .foregroundColor(.yellow)
+                            .font(.system(size: 25).weight(.light))
+                        Text("\(compScore)")
+                            .foregroundColor(.yellow)
+                            .font(.system(size: 50).weight(.bold))
+                    }
+                }
+                .padding()
+                .background(.black)
+                .clipShape(RoundedRectangle(cornerRadius: 10))
+                
+                
+                Text("Turns left: \(5-turns)")
+                    .foregroundColor(.white)
+                    .font(.system(size: 25).weight(.ultraLight))
+                
+                Spacer()
+                
+                VStack{
+                    Spacer()
+
+                    
+                    HStack{
+                        
+                        Button("🤜🏼"){
+                            // Rock
+                            userChoice(choice: 0)
+                        }
+                        .frame(minWidth: 100, minHeight: 100)
+                        .background(.regularMaterial)
+                        .font(.system(size: 100))
+                        .clipShape(Capsule())
+                        .shadow(radius: 5)
+                        
+                        
+                        Button("🖐🏼"){
+                            // Paper
+                            userChoice(choice: 1)
+                        }
+                        .frame(minWidth: 100, minHeight: 100)
+                        .background(.regularMaterial)
+                        .font(.system(size: 100))
+                        .clipShape(Capsule())
+                        .shadow(radius: 5)
+                        
+                        Button("✌🏼"){
+                            // Scissor
+                            userChoice(choice: 2)
+                        }
+                        .frame(minWidth: 100, minHeight: 100)
+                        .background(.regularMaterial)
+                        .font(.system(size: 100))
+                        .clipShape(Capsule())
+                        .shadow(radius: 5)
+                    }
+                    
+                    
+                    Spacer()
+                }
+                .frame(minWidth: 200, maxWidth: .infinity, minHeight: 200, maxHeight: 250)
+                .background(Color(red: 0.4, green: 0.25, blue: 0.6))
+                .clipShape(RoundedRectangle(cornerRadius: 50))
+                
+                Spacer()
+            }
+            .padding()
+            .alert("What happened?",isPresented: $isShowMove){
+                Button("Next Turn", action: nextTurn)
+            } message: {
+                Text("""
+                You played \(userOptions[userChoice])
+                Computer played \(counterOptions[compChoice])
+                \(turnStatus)
+                """)
+            }
+            .alert("Game Over", isPresented: $isGameOver){
+                Button("Restart", action: resetGame)
+            } message: {
+                Text("You have played your 5 turns. It's time to restart the game.")
+            }
         }
-        .padding()
+    }
+    
+    func userChoice(choice: Int){
+        compChoice = Int.random(in: 0...2)
+        userChoice = choice
+        if userOptions[userChoice] == counterOptions[compChoice]{
+            turnStatus = "It's a draw"
+        }
+        else if userChoice == compChoice{
+            compScore += 1
+            turnStatus = "Oops, you lost this round"
+        }
+        else {
+            userScore += 1
+            isturnWon = true
+            turnStatus = "You won this round"
+        }
+        isShowMove = true
+        turns += 1
+    }
+    
+    func nextTurn(){
+        isturnWon = false
+        if turns >= 5{
+            isGameOver = true
+        }
+    }
+    
+    func resetGame(){
+        turns = 0
+        userScore = 0
     }
 }
 
